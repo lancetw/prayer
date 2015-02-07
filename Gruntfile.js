939960498335cae86ea3b33d45f6c8dd31cfda76
Hsin-lin Cheng <lancetw@gmail.com>
2015-02-07 08:29:55 +0800

    move to staging server

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
         constants: {
           ENV: {
             name: 'development',
-            apiEndpoint: 'http://192.168.0.100:8000/api/v1/'
+            apiEndpoint: 'http://1and1.deliverwork.info/api/v1/'
           }
         }
       },
@@ -49,7 +49,7 @@ module.exports = function (grunt) {
         constants: {
           ENV: {
             name: 'production',
-            apiEndpoint: 'http://1and1.deliverwork.com/public/api/v1/'
+            apiEndpoint: 'http://1and1.ccea.org.tw/public/api/v1/'
           }
         }
       }
